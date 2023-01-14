a6370b574b3edacb49bd26f6daddb92762293cce
Yohann DC <YohannDCz@gmail.com>
2023-01-14 08:08:24 +0100

    Prise en charge de l'iframe header dans le finder.

---
+++ scripts/readyState.js
@@ -0,0 +1,7 @@
+// if (windowX.readyState == "loading") {
+      //   img.style.display = "flex";
+      //   iframe.style.display = "none";
+      // } else if (windowX.readyState == "complete") {
+      //   img.style.display = "none";
+      //   iframe.style.display = "flex"
+      // }
