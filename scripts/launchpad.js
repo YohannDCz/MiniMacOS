a78fa4a4fb5aefeb81679b8480b1e56e59f4fdfb
Yohann DC <YohannDCz@gmail.com>
2023-01-15 11:42:56 +0100

    Minor updates

--- scripts/launchpad.js
+++ scripts/launchpad.js
@@ -2,14 +2,14 @@ const iframeLaunchpad = document.getElementById('iframeLaunchpad');
 const launchpad = document.getElementById('launchpad');
 launchpad.onclick = function () {
   if (iframeLaunchpad.style.visibility === "hidden") {
-    iframeLaunchpad.style.zIndex = 0;
+    iframeLaunchpad.style.zIndex = 999;
     iframeLaunchpad.style.transform = "scale(1)";
     iframeLaunchpad.style.backdropFilter = "blur(20px)";
     iframeLaunchpad.style.opacity = 1;
     iframeLaunchpad.style.visibility = "visible";
     iframeLaunchpad.style.transition = "all 0.2s";
   } else if (iframeLaunchpad.style.visibility === "visible") {
-    iframeLaunchpad.style.zIndex = 999;
+    iframeLaunchpad.style.zIndex = 0;
     iframeLaunchpad.style.transform = "scale(1.2)";
     iframeLaunchpad.style.backdropFilter = "blur(0px)";
     iframeLaunchpad.style.opacity = 0;
